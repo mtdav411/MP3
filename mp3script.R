bc696267d2919cbb670ffa9ab61e542afdad624e
dmunoztord <dmunoztord@bonny.smith.edu>
2017-11-17 13:18:45 -0500

    mp3

--- mp3script.R
+++ mp3script.R
@@ -3,19 +3,24 @@ library(mdsr)
 library(RMySQL)
 db <- dbConnect_scidb(dbname = "imdb")
 
-western <- "SELECT t.id, t.title, t.production_year AS Year,mi1.info AS Genres
+western <- "SELECT t.id, t.title, t.production_year, mi1.info AS Genres
 FROM title t
 JOIN movie_info AS mi1 ON mi1.movie_id = t.id
-WHERE Year IS NOT NULL
+WHERE production_year IS NOT NULL
 AND t.kind_id = 1
 AND mi1.info_type_id = 3
-AND info = 'Western'
-AND Year
+AND mi1.info = 'Western'
+AND production_year
 BETWEEN 1900 AND 2015;
 "
 data <- db %>%
   dbGetQuery(western)
 
 View(data)
+
 ggplot(data, aes(production_year)) +
-  geom_histogram(aes(binwidth = range/10, fill = "#377EB8"))
+       geom_histogram(binwidth = 1)
+
+#ggplot(data, aes(production_year)) + geom_histogram()
+#ggplot(data, aes(production_year), binwidth = range/10) +
+  geom_histogram(aes(fill = "#377EB8"))
