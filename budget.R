dd0e4c2974481590c27ddef86b5e62c273d74dd3
dmunoztord <dmunoztord@bonny.smith.edu>
2017-12-08 12:03:50 -0500

    mp4

--- budget.R
+++ budget.R
@@ -1,4 +1,13 @@
-#Calculating the mean budget for 1950 and 2014
+library(tidyverse)
+library(scales)
+library(ggplot2)
+library(splitstackshape)
+library(mdsr)
+library(RMySQL)
+library(knitr)
+db <- dbConnect_scidb(dbname = "imdb")
+
+
 budget <- "SELECT t.id, t.title, t.production_year, mi2.info AS Gross, mi3.info AS Budget
 FROM title t
 JOIN movie_info AS mi1 ON mi1.movie_id = t.id
@@ -6,21 +15,22 @@ JOIN movie_info AS mi2 ON mi2.movie_id = t.id
 JOIN movie_info AS mi3 ON mi3.movie_id = t.id
 WHERE production_year IS NOT NULL
 AND t.kind_id = 1
-AND t.title IN ('Cowboys & Aliens', 'Rango', 'Django Unchained', 'The Revenant', 'High Noon', 'Shane', 'The Researchers', The Wild Bunch', 'Dances Wolves )
+AND t.title IN ('Cowboys & Aliens', 'Rango', 'Django Unchained', 'The Revenant', 'High Noon', 'Shane', 'The Researchers', 'The Wild Bunch', 'Dances with Wolves' )
 AND mi1.info_type_id = 3
 AND mi2.info_type_id = 107
 AND mi3.info_type_id = 105;
 "
 data_budget <- db %>%
   dbGetQuery(budget)
-View(data_budget)
-group
-
-data_budget_1950 <- data_budget %>% filter(production_year == 1950)
-data_budget_2014 <- data_budget %>% filter(production_year == 2014)
 
 
-# We then downloaded the data and uploaded it again so we could extract the numerical values from the "$" and the "," on the budget column
+data_budget <- cSplit(data_budget, "Gross", "(")
 
-Average_budget_2014 = 
-Average_budget_1950 = 
+
+
+
+data_budget_final <- data_budget %>%  mutate (Gross= parse_number(Gross_1), Budget= parse_number(Budget)) %>% group_by(id, title, production_year, Budget) %>% summarise(Gross=max(Gross)) %>% mutate (Profits = (Gross-Budget)) %>%
+rename("Title" = title, "Year" = production_year)
+
+kable(data_budget_final)
+
--- toptengross.R
+++ toptengross.R
@@ -7,23 +7,26 @@ library(RMySQL)
 db <- dbConnect_scidb(dbname = "imdb")
 
 
-toptengross <- "SELECT t.id, t.title, t.production_year, mi2.info AS Gross
+toptengross <- "SELECT t.id, t.title, t.production_year,mi1.info AS Genre, mi2.info AS Gross
 FROM title t
 JOIN movie_info AS mi1 ON mi1.movie_id = t.id
 JOIN movie_info AS mi2 ON mi2.movie_id = t.id
 WHERE production_year IS NOT NULL
 AND t.kind_id = 1
 AND mi1.info_type_id = 3
-AND mi2.info_type_id = 107;
+AND mi2.info_type_id = 107
+AND mi1.info = 'Western';
 "
 data_toptengross <- db %>%
   dbGetQuery(toptengross)
 
 
 data_toptengross <- cSplit(data_toptengross, "Gross", "(")
+View(data_toptengross)
 
+data_toptengross_final <- data_toptengross %>%  mutate (Gross= parse_number(Gross_1)) %>% group_by(id, title, production_year) %>% summarise(Gross=max(Gross)) %>% arrange(desc(Gross)) %>% filter(between(row_number(), 1, 10))
 
-data_toptengross_final <- data_toptengross %>%  mutate (Gross= parse_number(Gross_1), Budget= parse_number(Budget)) %>% select("id", "title", "production_year", "Budget", "Gross") %>% group_by(id, title, production_year, Budget) %>% summarise(Gross=max(Gross)) %>% mutate (profits = (Gross-Budget))
+data_toptengross_final_1 <- head(data_toptengross_final,10)
 
-kable(data_toptengross_final)
+kable(data_toptengross_final_1)
 
